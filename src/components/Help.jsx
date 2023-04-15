3e8ea6ba11e367bb3d18e6f89179b2cb802419d7
malav22 <malavakhani58@gmail.com>
2023-04-15 23:45:51 +0530

    help page modified

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,61 +1,96 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 
-function Help () {
+function Help() {
 
     return (
-      <div  style={{display:'flex',flexDirection:'column',
-                    background: '#ECE8F0',
-                    background: '-webkit-linear-gradient(top left, #ECE8F0, #4A249F)',
-                    background: '-moz-linear-gradient(top left, #ECE8F0, #4A249F)',
-                    background: 'linear-gradient(to bottom right, #ECE8F0, #4A249F)'}}>
-        <h1 style={{margin:'1rem auto',  
-                    fontSize: '70px',
-                     fontWeight:'600',
-                    backgroundImage: 'linear-gradient(to right, #553c9a, #AD3E25)',
-                    color: 'transparent',
-                    backgroundClip: 'text',
-                    webkitBackgroundClip: 'text'}}>How to use this website</h1>
-            <div style={{border:'solid 1px black', margin:'2rem auto',minHeight:'100px',minWidth:'1000px',padding:'10px',justifyItems:'center',boxShadow:'0 8px 8px -4px lightblue',background:'#eee'}}>
-               <ul>
-               <li>You can begin using medEZ , once you login with your google account. </li>
-               <li>Grant permission to your calender to enable dosage updates on your connected devices</li> 
-               </ul>
-            </div>
-            <div style={{border:'solid 1px black', margin:'2rem auto',minHeight:'100px',minWidth:'1000px',padding:'10px',justifyItems:'center',boxShadow:'0 8px 8px -4px lightblue',background:'#eee'}}>
-                There are a 3 functional sections in the navigation bar
-                <ol>
-                    <li>Upload</li>
-                    <li>Search</li>
-                    <li>Calender</li>
-                </ol>
-                <p>Let us dive into these one by one </p>
-            </div>
-            <div style={{border:'solid 1px black', margin:'2rem auto',minHeight:'100px',minWidth:'1000px',padding:'10px',justifyItems:'center',boxShadow:'0 8px 8px -4px lightblue',background:'#eee'}}>
-                <h3>Upload</h3>
-                <p>
-                   1.  Click on the upload button in your navbar. It will ask you to choose a file. <br/>
-                   2.  Upload your prescription as an image or a pdf file. <br/>
-                   3.  medEZ extracts the medicine names from the prescription once you hit submit.<br/>
-                   4.  Hit compare to reach the page to actually buy medicines.<br />
-                   5.  You will be given a drop down list with the names of the medicines, toggle between medicines from this list<br/>
-                </p>
-            </div>
-            <div style={{border:'solid 1px black', margin:'2rem auto',minHeight:'100px',minWidth:'1000px',padding:'10px',justifyItems:'center',boxShadow:'0 8px 8px -4px lightblue',background:'#eee'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', }}>
+            <h1 style={{
+                margin: '1rem auto',
+                fontSize: '70px',
+                fontWeight: '600',
+                backgroundImage: 'linear-gradient(to right, #553c9a, #AD3E25)',
+                color: 'transparent',
+                backgroundClip: 'text',
+                webkitBackgroundClip: 'text'
+            }}>Guide to use MedEZ</h1>
+            <div style={{ margin: '1rem auto', width: '80%', }}>
+                <div style={{ margin: '1rem auto',}} className="gradient-border-rounded">
 
-                <h3>Search</h3>
-                <p>
-                    In this section , you can manually type the name of a medicine and medEZ will fetch it's best prices across the internet.
-                </p>
-            </div>
-            <div style={{border:'solid 1px black', margin:'2rem auto',minHeight:'100px',minWidth:'1000px',padding:'10px',justifyItems:'center',boxShadow:'0 8px 8px -4px lightblue',background:'#eee'}}>
+                    <p>You can begin using medEZ , once you login with your google account.<br />
+                        Grant permission to your Google calender to enable dosage updates on your connected devices</p>
+                    There are 3 functional sections in the navigation bar
+                    <ol>
+                        <li>Upload</li>
+                        <li>Search</li>
+                        <li>Calender</li>
+                    </ol>
+                </div>
+                <div className="row gradient-border-rounded" style={{ textAlign: 'center' }}>
+                    <h2>Upload</h2>
+                    <hr style={{ border: '2px solid', height: '0', width: '50%', margin: '0 25%' }} />
+                    <br />
+                    <div className="column1" >
+                        <img src="./1.png" alt="pic" style={{ width: '150px',borderRadius:'50%' }} />
+                    </div>
+                    <div className="column2" >
+                        <div style={{ textAlign: 'center' }}>
 
-                <h3>Calender</h3>
-                <p>
-                    Use the calender to schedule updates and dosage reminders for your medication.
-                </p>
+
+                            <p style={{ textAlign: 'left' }}>
+                                <ul>
+                                    <li>
+                                        Click on the upload button in your navbar. It will ask you to choose a file.
+                                    </li>
+                                    <li>
+                                        Upload your prescription as an image or a pdf file.
+                                    </li>
+                                    <li>
+                                        medEZ extracts the medicine names from the prescription once you hit submit.
+                                    </li>
+                                    <li>
+                                        Hit compare to reach the page to actually buy medicines.
+                                    </li>
+                                    <li>
+                                        You will be given a drop down list with the names of the medicines, toggle between medicines from this list.
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div className="row" style={{ textAlign: 'center' }}>
+
+                    <div className="column gradient-border-rounded" >
+                        <h2>Search</h2>
+                        <hr style={{ border: '2px solid', height: '0', width: '50%', margin: '0 25%' }} />
+                        <br />
+                        <div style={{ textAlign: 'center' }}>
+
+
+                            <p style={{ textAlign: 'left' }}>
+                                In this section , you can manually type the name of a medicine and medEZ will fetch it's best prices across the internet.
+                            </p>
+                        </div>
+                    </div>
+                    <br />
+                    <div className="column gradient-border-rounded" >
+                        <h2>Calendar</h2>
+                        <hr style={{ border: '2px solid', height: '0', width: '50%', margin: '0 25%' }} />
+                        <br />
+                        <div style={{ textAlign: 'center' }}>
+
+
+                            <p style={{ textAlign: 'left' }}>
+                                Use the calender to schedule updates and dosage reminders for your medication.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-    </div>
+        </div>
     )
 }
 
